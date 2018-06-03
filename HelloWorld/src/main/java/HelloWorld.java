b5b89517672c9f5de153d2b0e7577a9ba29578a0
YaFei-Li <627208969@qq.com>
2018-06-03 13:47:57 +0800

    add "haha"

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello wold!");
         System.out.println("Test push branch dev-lyf");
+        System.out.println("haha");
     }
 }
